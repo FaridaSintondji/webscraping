f47a5ab76878f53766a148282f6918973abb9c51
FaridaSintondji <fari.sintondji@gmail.com>
2025-12-04 17:38:49 +0100

    Modification de poduits_decitre.py

--- comparateur-prix/scraper/comparateur/comparateur/spiders/produits_decitre.py
+++ comparateur-prix/scraper/comparateur/comparateur/spiders/produits_decitre.py
@@ -3,8 +3,16 @@ import scrapy
 
 class ProduitsDecitreSpider(scrapy.Spider):
     name = "produits_decitre"
-    allowed_domains = ["comparateur"]
-    start_urls = ["https://comparateur"]
+    allowed_domains = ["www.decitre.fr/"]
+    start_urls = ["https://www.decitre.fr/"]
 
     def parse(self, response):
-        pass
+        books = response.xpath('//div[@class="product-card-infos"]')
+        for book in books:
+            titre = book.xpath('./a/h3[@class="product-card-infos__details__texts__link__title"]').get()
+            prix = book.xpath('./div[@class="price"]/text()').get()
+
+            yield{
+                'titre': titre,
+                'prix': prix
+            }
