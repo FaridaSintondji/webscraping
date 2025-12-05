e21b44415fbdecbc38a9032ec8f2c32f4a399387
FaridaSintondji <fari.sintondji@gmail.com>
2025-12-05 18:11:27 +0100

    Ajout des xpath dans le spider, modif de item et intégration de item dans le fichier spider

--- comparateur-prix/scraper/comparateur/comparateur/items.py
+++ comparateur-prix/scraper/comparateur/comparateur/items.py
@@ -9,4 +9,6 @@ import scrapy
 class ComparateurItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    titre = scrapy.Field()
+    prix = scrapy.Field()
+    lien = scrapy.Field()
--- comparateur-prix/scraper/comparateur/comparateur/spiders/produits_decitre.py
+++ comparateur-prix/scraper/comparateur/comparateur/spiders/produits_decitre.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from ..items import ComparateurItem
 
 class ProduitsDecitreSpider(scrapy.Spider):
     name = "produits_decitre"
@@ -7,12 +7,12 @@ class ProduitsDecitreSpider(scrapy.Spider):
     start_urls = ["https://www.decitre.fr/"]
 
     def parse(self, response):
+        
         books = response.xpath('//div[@class="product-card-infos"]')
+        
         for book in books:
-            titre = book.xpath('./a/h3[@class="product-card-infos__details__texts__link__title"]').get()
-            prix = book.xpath('./div[@class="price"]/text()').get()
-
-            yield{
-                'titre': titre,
-                'prix': prix
-            }
+            item = ComparateurItem()
+            item['titre'] = book.xpath('./div[@class="product-card-infos__details"]/div[@class="product-card-infos__details__texts"]/a[@class="product-card-infos__details__texts__link link--active"]/h3[@class="product-card-infos__details__texts__link__title"]/text()').get()
+            item['prix'] = book.xpath('./div[@class="product-card-price"]/div[@class="price"]/text()').get()
+            item['lien'] = book.xpath('./div[@class="product-card-infos__details"]/div[@class="product-card-infos__details__texts"]/a/@href').get()
+            yield item
